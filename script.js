19486c966a6fd990455dd7a818268ceb84150871
Vipu <lessandsass@gmail.com>
2025-07-20 18:00:24 +0530

    create a seperate script file and move the existing logic

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+    document.querySelector(".sidebar").style.display = "flex";
+}
+function hideSidebar() {
+    document.querySelector(".sidebar").style.display = "none";
+}
+
+
